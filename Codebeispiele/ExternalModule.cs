7651260de4668ed630fab605431eb2575e81e205
Dominik Steffen <dominik.steffen@googlemail.com>
2013-07-19 13:29:58 +0200

    More work.

---
+++ Codebeispiele/ExternalModule.cs
@@ -0,0 +1,5 @@
+/* 
+	The following method header would apply 
+	a transformation matrix to a geometry object.
+*/
+public static bool Transform(float4x4 transformationMatrix, ref Geometry geo) {...}
